0686e7f401639e775ab5030f3458e297632b5874
Masen Matthews <masenmatthews@gmail.com>
2018-03-16 10:16:50 -0700

    draft first failing test

--- spec/time-spec.js
+++ spec/time-spec.js
@@ -1,10 +1,1 @@
 import { Time } from './../js/time.js';
-
-describe('Time', function() {
-  it("should take the user's birthday and return it in years and seconds", function() {
-    let birthday = new Time(12/19/1994)
-    expect(birthday.getFullYear()).toEqual(23);
-    expect(birthday.getSeconds()).toEqual(725328000);
-  });
-
-});
--- spec/user-spec.js
+++ spec/user-spec.js
@@ -1,1 +1,15 @@
 import { User } from './../js/user.js';
+
+describe('User', function() {
+  let human;
+
+  beforeEach(function() {
+  human = new User(12/19/1994);
+  });
+
+  it("should take the user's birthday and return it in years and seconds", function() {
+    let human = new User(12/19/1994)
+    expect(human.getFullYear()).toEqual(23);
+    expect(human.getSeconds()).toEqual(725328000);
+  });
+});
