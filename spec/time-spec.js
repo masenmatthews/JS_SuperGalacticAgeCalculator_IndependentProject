bbeb50dfd9f11945e071c6932c8824eeb5a9da2e
Masen Matthews <masenmatthews@gmail.com>
2018-03-16 09:34:47 -0700

    begin drafting specs

---
+++ spec/time-spec.js
@@ -0,0 +1,10 @@
+import { Time } from './../js/time.js';
+
+describe('Time', function() {
+
+  it('should ________________l', function() {
+    let  = new Time()
+    expect(VARIABLE.PROTOTYPE()).toEqual("");
+  });
+
+});
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import {Time} from '../js/time.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
